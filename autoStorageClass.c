622a880bc3bc01b7e84d5f265f8d68d8dac0f639
nitinbhopale <nitinbhopale53@gmail.com>
2023-01-08 14:01:24 +0530

    message

---
+++ autoStorageClass.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int no;         // auto storage class yala default memory hi garbage bhetate.jar data type chya 
+                    // adhi jar storage class ch name specify kel nahi tar tya case madhe by default tyala auto storage class madhe memory alloacate hote.
+    printf("%d",no);
+    return 0;
+}
